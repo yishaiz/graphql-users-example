66cd7ed63e2a65257c8e6f0986ede811b946f4bb
yishai <yishai5555@gmail.com>
2026-08-28 13:40:04 +0300

    04-schema

---
+++ users/schema/schema.js
@@ -0,0 +1,17 @@
+const graphql = require('graphql')
+
+
+const {
+    GraphQLObjectType,
+    GraphQLString
+} = graphql
+
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: graphql.GraphQLInt }
+    }
+})
--- users/server.js
+++ users/server.js
@@ -1,8 +1,24 @@
 const epxress = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
 
 const app = epxress()
 const PORT = 4000
 
+
+app.use('/graphQL', expressGraphQL({
+    graphiql: true
+}))
+
 app.listen(PORT, () => {
     console.log(`app is listening to http://localhost:${PORT}`)
 })
+
+
+// todo:
+// Change this:
+
+// const expressGraphQL = require('express-graphql');
+
+// to this:
+
+// const expressGraphQL = require('express-graphql').graphqlHTTP; 
